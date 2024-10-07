6a3311f998d32f54db1558c35cad2c87307e4748
David Echenagucia <davidleonardo220@gmail.com>
2024-10-07 13:50:52 -0500

    Proyecto terminado de Nextjs y bon base de datos Prisma

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+function AboutPage() {
+    return (
+        <section className= "container mx-auto">
+            <h3 className="text-4xl font-bold my-5">
+                About
+            </h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat natus tempora consequuntur repellendus, tempore error iure laboriosam? Quae, officia. Tenetur neque unde sapiente corporis ab, eveniet rerum optio qui earum tempore quidem non ullam iure blanditiis! Deleniti magni animi quos dignissimos molestias aut tenetur saepe aspernatur optio earum. Adipisci soluta necessitatibus eum quis hic similique doloremque ipsam ipsa obcaecati? Excepturi amet commodi expedita veritatis praesentium minima repudiandae debitis, eius voluptates velit tempore dolore, aut facere distinctio quae, similique quisquam eum quibusdam aspernatur quo. Explicabo quasi mollitia suscipit expedita tenetur cumque totam odit magni et. Eius voluptatem debitis enim quia dolore?
+            </p>
+        </section>
+    )
+}
+
+export default AboutPage;
